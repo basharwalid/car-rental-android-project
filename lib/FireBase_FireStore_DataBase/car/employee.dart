3fa53a8e608e8f586daaaecba7b87d944ecdd73b
john safwat <john362sss@gmail.com>
2022-12-17 07:21:10 +0200

    add employee class

---
+++ lib/FireBase_FireStore_DataBase/car/employee.dart
@@ -0,0 +1,30 @@
+class Employee {
+  String EmployeeID ;
+  int    EmployeeDepartment ;
+  String EmployeeName ;
+  String EmployeeEmail ;
+  String EmployeePassword ;
+  String EmployeeAge ;
+  String EmployeeNationalID ;
+  String EmployeePhoneNumber ;
+  String EmployeeCountry ;
+  String EmployeeCity ;
+  double EmployeeSalary ;
+  String Image ;
+
+  Employee ({
+    required this.EmployeeID ,
+    required this.EmployeeDepartment ,
+    required this.EmployeeName ,
+    required this.EmployeeEmail ,
+    required this.EmployeePassword ,
+    required this.EmployeeAge ,
+    required this.EmployeeNationalID ,
+    required this.EmployeePhoneNumber ,
+    required this.EmployeeCountry ,
+    required this.EmployeeCity ,
+    required this.EmployeeSalary ,
+    required this.Image
+  });
+
+}
